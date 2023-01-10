0a5a6ae17cdafe542f19bb337a058dfb20a093fc
Yacine667 <sid.yacinee@gmail.com>
2023-01-10 15:01:50 +0100

    modif FormationController pour afficher toutes les formations dans la vue

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Formation;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FormationController extends AbstractController
 {
     #[Route('/formation', name: 'app_formation')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        // Récuperer les Formations qu'il y a en base de données
+
+        $formations = $doctrine->getRepository(Formation::class)->findAll();
         return $this->render('formation/index.html.twig', [
-            'controller_name' => 'FormationController',
+            'formations' => $formations
         ]);
     }
 }
